251d6fb0c7290ea3468a9e9943cd979ba120b1ef
teddyhn <tngo97@gmail.com>
2019-09-13 09:08:30 -0700

    Initial commit

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,59 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { getSmurfs, postSmurfs } from "../actions";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
 
-export default App;
+const App = ({ getSmurfs, postSmurfs, smurfs, isFetching }) => {
+  const [newSmurf, setNewSmurf] = useState({})
+
+  const handleChanges = e => {
+    setNewSmurf(e.target.value);
+  };
+
+  useEffect(() => {
+    getSmurfs();
+  }, [getSmurfs]);
+
+  if (isFetching) {
+    return <h3>Retrieving smurfs...</h3>;
+  }
+
+  return (
+    <div className="App">
+      <div className="smurf-form">
+          <input
+              className="smurf-input-name"
+              type="text"
+              name="newSmurfName"
+              value={newSmurf}
+              onChange={handleChanges}
+          />
+          <button
+              onClick={postSmurfs}
+          >
+              Add new smurf
+          </button>
+      </div>
+      {smurfs.map(smurf => (
+        <div className="smurf">
+          <p>Name: {smurf.name}</p>
+          <p>Age: {smurf.age}</p>
+          <p>Height: {smurf.height}</p>
+        </div>
+    ))}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSmurfs, postSmurfs }
+)(App);
